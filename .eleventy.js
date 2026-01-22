f3ca7371d46bd5ff2bcf01420c813a1bcd1b5c9b
msugawarab <m.sugawara.b@gmail.com>
2026-01-22 18:44:21 +0900

    修正：正しいリンクが生成されるように

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,12 @@
 module.exports = function(eleventyConfig) {
+    // 日付を YYYY-MM-DD 形式にするためのフィルタを追加
+    eleventyConfig.addFilter("date", (dateObj) => {
+      const d = new Date(dateObj);
+      return d.toISOString().split('T')[0];
+    });
+  
     return {
-      pathPrefix: "/my-essays/", // リポジトリ名を指定
+      pathPrefix: "/my-essays/", // GitHub Pages用の設定
       dir: {
         input: ".",
         includes: "_includes",
